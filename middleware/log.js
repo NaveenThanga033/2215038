528708bdd0947e9648a57446402ba02e84baca78
Thanga naveen B <141567404+NaveenThanga033@users.noreply.github.com>
2025-08-20 12:34:38 +0530

    Update log.js

--- middleware/log.js
+++ middleware/log.js
@@ -4,7 +4,9 @@ const LOG_API="http://20.244.56.144/evaluation-service/logs";
 
 export async function log(stack,level,pkg,message) {
   try {
-    await axios.post(LOG_API,{stack,level,package:pkg,message });
+    await axios.post(LOG_API,{stack,level,package:pkg,message },{
+      header:{ Authorization: `Bearer ${process.env.ACCESS_TOKEN}`}
+    });
   } catch (error) {
     console.error("Log service error:",error.message);
   }
